145524ff1bfec8919029419dd96157abe40d2582
DiceyGuy <158840173+DiceyGuy@users.noreply.github.com>
2025-06-30 20:44:22 +0200

    FIX: Clean package.json BOM/encoding issues

--- config/webpack/webpack.prod.js
+++ config/webpack/webpack.prod.js
@@ -1,216 +1,44 @@
-// Update config/webpack/webpack.prod.js for better bundle optimization
-
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin'); // ← ADDED THIS
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
-const CompressionPlugin = require('compression-webpack-plugin');
-
-module.exports = {
-  mode: 'production',
-  entry: {
-    main: './src/index.js'
+{
+  "name": "mtg-scanner-pro",
+  "version": "1.0.0",
+  "description": "AI-powered MTG card scanner with 98% accuracy",
+  "main": "src/index.js",
+  "engines": {
+    "node": "18.x",
+    "npm": "9.x"
   },
-  output: {
-    path: path.resolve(__dirname, '../../dist'),
-    filename: '[name].[contenthash:8].js',
-    chunkFilename: '[name].[contenthash:8].chunk.js',
-    publicPath: '/',
-    clean: true,
-    assetModuleFilename: 'assets/[name].[contenthash:8][ext]'
+  "scripts": {
+    "start": "react-scripts start",
+    "build": "react-scripts build",
+    "test": "react-scripts test",
+    "eject": "react-scripts eject",
+    "dev": "react-scripts start"
   },
-  resolve: {
-    extensions: ['.js', '.jsx', '.json'],
-    alias: {
-      '@': path.resolve(__dirname, '../../src'),
-      'react': path.resolve(__dirname, '../../node_modules/react'),
-      'react-dom': path.resolve(__dirname, '../../node_modules/react-dom')
-    }
+  "dependencies": {
+    "react": "^18.2.0",
+    "react-dom": "^18.2.0",
+    "react-scripts": "5.0.1",
+    "web-vitals": "^3.3.2"
   },
-  optimization: {
-    minimize: true,
-    minimizer: [
-      new TerserPlugin({
-        terserOptions: {
-          compress: {
-            drop_console: true,
-            drop_debugger: true,
-            pure_funcs: ['console.log', 'console.info', 'console.debug']
-          },
-          mangle: true,
-          format: {
-            comments: false
-          }
-        },
-        extractComments: false
-      }),
-      new CssMinimizerPlugin()
-    ],
-    splitChunks: {
-      chunks: 'all',
-      minSize: 20000,
-      maxSize: 250000,
-      cacheGroups: {
-        default: {
-          minChunks: 2,
-          priority: -20,
-          reuseExistingChunk: true
-        },
-        vendor: {
-          test: /[\\/]node_modules[\\/]/,
-          name: 'vendors',
-          priority: -10,
-          chunks: 'all',
-          enforce: true
-        },
-        react: {
-          test: /[\\/]node_modules[\\/](react|react-dom)[\\/]/,
-          name: 'react',
-          priority: 20,
-          chunks: 'all',
-          enforce: true
-        },
-        tesseract: {
-          test: /[\\/]node_modules[\\/]tesseract\.js[\\/]/,
-          name: 'tesseract',
-          priority: 15,
-          chunks: 'all',
-          enforce: true
-        },
-        common: {
-          name: 'common',
-          minChunks: 2,
-          priority: 5,
-          chunks: 'all',
-          enforce: true
-        }
-      }
-    },
-    runtimeChunk: {
-      name: 'runtime'
-    },
-    usedExports: true,
-    sideEffects: false
+  "devDependencies": {
+    "@babel/plugin-proposal-private-property-in-object": "^7.21.11"
   },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              ['@babel/preset-env', {
-                targets: '> 0.25%, not dead',
-                modules: false,
-                useBuiltIns: 'usage',
-                corejs: 3
-              }],
-              ['@babel/preset-react', { runtime: 'automatic' }]
-            ],
-            plugins: [
-              '@babel/plugin-syntax-dynamic-import',
-              ['@babel/plugin-transform-runtime', {
-                regenerator: true
-              }]
-            ]
-          }
-        }
-      },
-      {
-        test: /\.css$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          {
-            loader: 'css-loader',
-            options: {
-              importLoaders: 1,
-              modules: false
-            }
-          },
-          {
-            loader: 'postcss-loader',
-            options: {
-              postcssOptions: {
-                plugins: [
-                  ['tailwindcss', {}],
-                  ['autoprefixer', {}],
-                  ['cssnano', {
-                    preset: ['default', {
-                      discardComments: { removeAll: true }
-                    }]
-                  }]
-                ]
-              }
-            }
-          }
-        ]
-      },
-      {
-        test: /\.(png|jpe?g|gif|svg|ico)$/i,
-        type: 'asset',
-        parser: {
-          dataUrlCondition: {
-            maxSize: 8 * 1024 // 8kb
-          }
-        }
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
-        generator: {
-          filename: 'fonts/[name].[contenthash:8][ext]'
-        }
-      }
+  "eslintConfig": {
+    "extends": [
+      "react-app",
+      "react-app/jest"
     ]
   },
-  plugins: [
-    // ← ADDED HtmlWebpackPlugin FIRST
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-      filename: 'index.html',
-      inject: true,
-      minify: {
-        removeComments: true,
-        collapseWhitespace: true,
-        removeRedundantAttributes: true,
-        useShortDoctype: true,
-        removeEmptyAttributes: true,
-        removeStyleLinkTypeAttributes: true,
-        keepClosingSlash: true,
-        minifyJS: true,
-        minifyCSS: true,
-        minifyURLs: true,
-      }
-    }),
-    new MiniCssExtractPlugin({
-      filename: '[name].[contenthash:8].css',
-      chunkFilename: '[name].[contenthash:8].css'
-    }),
-    new CompressionPlugin({
-      algorithm: 'gzip',
-      test: /\.(js|css|html|svg)$/,
-      threshold: 8192,
-      minRatio: 0.8
-    }),
-    // Uncomment to analyze bundle size
-    // new BundleAnalyzerPlugin({
-    //   analyzerMode: 'static',
-    //   openAnalyzer: false,
-    //   reportFilename: 'bundle-report.html'
-    // })
-  ],
-  performance: {
-    maxAssetSize: 512000,
-    maxEntrypointSize: 512000,
-    hints: 'warning'
-  },
-  stats: {
-    preset: 'minimal',
-    moduleTrace: true,
-    errorDetails: true
+  "browserslist": {
+    "production": [
+      ">0.2%",
+      "not dead",
+      "not op_mini all"
+    ],
+    "development": [
+      "last 1 chrome version",
+      "last 1 firefox version",
+      "last 1 safari version"
+    ]
   }
-};
+}
